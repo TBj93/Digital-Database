25b63d1bb711c5f22749849a3bb937eabbcc709f
TBj93 <Tim-Burmester1@web.de>
2022-05-03 19:21:17 +0200

    Created a pokelist
    Pokelist using appendChild and created query elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,64 @@
-let pokemonRepository = (function () { 
-
-let pokemonList = [
-  {
-    name: "Mewtwo",
-    height: 2,
-    types: ["Psychic"],
-  },
-  {
-    name: "Aerodactyl",
-    height: 1.8, 
-    types: ["Rock", "Flying"],
-  },
-  {
-    name: "Gyarados",
-    height: 6.1,
-    types: ["Water", "Flying"],
-  },
-];
+let pokemonRepository = (function () {
+///addListItem()
 
 
+  let pokemonList = [
+    {
+      name: "Mewtwo",
+      height: 2,
+      types: ["Psychic"],
+    },
+    {
+      name: "Aerodactyl",
+      height: 1.8,
+      types: ["Rock", "Flying"],
+    },
 
- function getAll() {
+    {
+      name: "Gyarados",
+      height: 6.1,
+      types: ["Water", "Flying"],
+    },
+  ];
+
+  function getAll() {
     return pokemonList;
-}
-// return all items
- function add(pokemon)  {
+  }
+  // return all items
+  function add(pokemon) {
     PokemonList.push(pokemon);
-}
-//add a single item to the pokemonLst
-return {
+  }
+  //add a single item to the pokemonLst
+  return {
     add: add,
-    getAll: getAll
-};    
-    
+    getAll: getAll,
+  };
 })();
 
 //  document.write(pokemonRepository.getAll());
 
+pokemonRepository.getAll().forEach(function (pokemon) {
+  // writes every pokemon in the list + height
+///create button
+let pokelist = document.querySelector('.pokelist');
+let listItem = document.createElement('li');
+let button = document.createElement('button');  
+
+ button.innerText = pokemon.name;
+
+ button.classList.add('button');
+listItem.appendChild(button);
+pokelist.appendChild(listItem);
+  
+});
 
 
-pokemonRepository.getAll().forEach(function(pokemon){
- // writes every pokemon in the list + height 
 
-         if(pokemon.height > 4 ){
-            document.write(pokemon.name+'__height:'+pokemon.height+'--> Huge!'+'<br>  </br>');
-        }
-        
-        else{
-        document.write(pokemon.name+'__height:'+pokemon.height+'<br>  </br>');
-    }           
-
-                    });
+/*  if (pokemon.height > 4) {
+  document.write(
+    pokemon.name + "__height:" + pokemon.height + "--> Huge!" + "<br>  </br>"
+  );
+} else {
+  document.write(pokemon.name + "__height:" + pokemon.height + "<br>  </br>");
+}
+  */
