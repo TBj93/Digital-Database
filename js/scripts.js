b4e4e2185114029e634281b4149f9890b52bc983
TBj93 <Tim-Burmester1@web.de>
2022-05-08 17:32:40 +0200

    Added pokeURL via JSON

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "Mewtwo",
-      height: 2,
-      types: ["Psychic"],
-    },
-    {
-      name: "Aerodactyl",
-      height: 1.8,
-      types: ["Rock", "Flying"],
-    },
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-    {
-      name: "Gyarados",
-      height: 6.1,
-      types: ["Water", "Flying"],
-    },
-  ];
+  function LoadList() {
+{
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+  }
+  function loadDetails(pokemon) {
+    let url = pokemon.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  
 
   function getAll() {
     return pokemonList;
@@ -28,7 +45,9 @@ let pokemonRepository = (function () {
   //add a single item to the pokemonLst
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
   // shows details about the object
 
@@ -50,13 +69,38 @@ let pokemonRepository = (function () {
   return {
     add: add,
     getAll: getAll,
+    LoadList: LoadList,
+    loadDetails: loadDetails,
     addListItem: addListItem,
     showDetails: showDetails,
   };
 })();
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  // writes every pokemon in the list
-  pokemonRepository.addListItem(pokemon);
-  // adds button
+pokemonRepository.LoadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
+
+
+/*
+
+    {
+      name: "Mewtwo",
+      height: 2,
+      types: ["Psychic"],
+    },
+    {
+      name: "Aerodactyl",
+      height: 1.8,
+      types: ["Rock", "Flying"],
+    },
+
+    {
+      name: "Gyarados",
+      height: 6.1,
+      types: ["Water", "Flying"],
+    },
+  
+    */
